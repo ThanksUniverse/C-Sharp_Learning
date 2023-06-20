93bcd953c7a96341897654eff922e349007ab917
Pedro <ThanksUniverse333@outlook.com>
2023-06-20 12:28:55 -0300

    first commit

---
+++ LinqUsage/Book.cs
@@ -0,0 +1,37 @@
+﻿namespace LinqUsage
+{
+    class Book
+    {
+        private string Title { get; set; }
+        private int Pages { get; set; }
+        private double Price { get; set; }
+        public Book()
+        {
+            Title = "Sample";
+            Pages = 0;
+            Price = 0.0;
+            Console.WriteLine("You created a new sample book.");
+        }
+
+        public Book(string title, int pages, double price)
+        {
+            Title = title;
+            Pages = pages;
+            Price = price;
+            Console.WriteLine("You created a new book:\nTitle: {0}\nPages: {1}\nPrice: {2}", title, pages, price);
+        }
+
+        public string GetName() 
+        {
+            return Title;
+        }
+
+        public double GetPrice()
+        {
+            return Price;
+        }
+
+
+
+    }
+}
---
+++ LinqUsage/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace LinqUsage
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var myBooks = new List<Book>();
+            Book SampleBook = new("C# Basics", 100, 3.8);
+            Book SampleBook2 = new("C# Advanced", 200, 7.4);
+            Book SampleBook3 = new("Extremely Advanced Programming", 300, 10.0);
+            Book SampleBook4 = new("Basic Lua Programming", 50, 1.5);
+
+            myBooks.Add(SampleBook); myBooks.Add(SampleBook2); myBooks.Add(SampleBook3); myBooks.Add(SampleBook4);
+
+            var OrderedBooks = myBooks.Where(a => a.GetName().Contains("C#") || a.GetName().Contains("Lua")).OrderBy(a => a.GetPrice());
+
+            foreach (Book book in OrderedBooks)
+            {
+                Console.WriteLine(book.GetName());
+            }
+
+
+        }
+    }
+}
