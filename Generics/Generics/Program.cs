5cc14c4c5fb46ceed282a1afff5488b32355862c
Pedro <ThanksUniverse333@outlook.com>
2023-06-27 13:01:28 -0300

    Generics

---
+++ Generics/Generics/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main()
+        {
+            var person = new Person();
+            var payment = new Payment();
+            var subscription = new Subscription();
+            var context = new DataContext<Person, Payment, Subscription>();
+            context.Save(person);
+        }
+    }
+
+    public class DataContext<P, PA, S>
+        where P : IPerson
+        where PA : Payment
+        where S : Subscription
+    {
+        public void Save(P entity)
+        {
+
+        }
+
+        public void Save(PA entity)
+        {
+
+        }
+
+        public void Save(S entity)
+        {
+
+        }
+    }
+
+    public class Person : IPerson
+    {
+
+    }
+
+    public class Payment
+    {
+
+    }
+
+    public class Subscription
+    {
+
+    }
+
+    public interface IPerson
+    {
+
+    }
+}
