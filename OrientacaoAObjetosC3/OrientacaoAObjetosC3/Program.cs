b67944dc098ea3fceea749abd9317cfc7f922526
Pedro <ThanksUniverse333@outlook.com>
2023-06-27 11:45:55 -0300

    Orientacao a Objetos Part III

--- OrientacaoAObjetosC3/OrientacaoAObjetosC3/Program.cs
+++ OrientacaoAObjetosC3/OrientacaoAObjetosC3/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace PartIII
+{
+    public class Learning
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello World");
+
+            Pessoa pessoa = new Pessoa(1, "Pedro");
+            var pessoa2 = new Pessoa(1, "Pedro");
+
+            Console.WriteLine(pessoa == pessoa2);
+            Console.WriteLine(pessoa.Name == pessoa2.Name);
+            Console.WriteLine(pessoa);
+            Console.WriteLine(pessoa2);
+            Console.WriteLine(pessoa.Equals(pessoa2));
+
+            Console.WriteLine(pessoa.Equals(pessoa2)
+        }
+    }
+
+    public class Pessoa : IEquatable<Pessoa>
+    {
+        public string? Name { get; set; }
+        public int Id { get; set; }
+        public Pessoa(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public bool Equals(Pessoa? other)
+        {
+            return this.Id == other.Id;
+        }
+    }
+}
+
