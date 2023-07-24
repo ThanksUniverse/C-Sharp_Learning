56fddfcb733a8161507c300eda0d84ebfee70665
Pedro <ThanksUniverse333@outlook.com>
2023-07-23 21:38:54 -0300

    MVC Start

---
+++ mvcCodding/Sistema.DAO/UsuarioDAO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sistema.Model;
+
+namespace Sistema.DAO
+{
+    public class UsuarioDAO
+    {
+
+    }
+}
