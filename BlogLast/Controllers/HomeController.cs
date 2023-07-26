a852f40d6c191c500d42f3abdeb2521d016c2c54
Pedro <ThanksUniverse333@outlook.com>
2023-07-26 06:22:59 -0300

    Last Updates

---
+++ BlogLast/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// Health Check
+namespace BlogLast.Controllers
+{
+    [ApiController]
+    [Route("")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet("")]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ BlogLast/Program.cs
@@ -0,0 +1,12 @@
+using Blog.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<BlogDataContext>();
+
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
