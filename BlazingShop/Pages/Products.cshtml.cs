721204b0f6760d1ea87dec968f2a1171e51202fb
Pedro <ThanksUniverse333@outlook.com>
2023-08-06 16:16:29 -0300

    BlazorServer

---
+++ BlazingShop/Pages/Products.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BlazingShop.Pages;
+
+public class Products : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
--- BlazingShop/Program.cs
+++ BlazingShop/Program.cs
@@ -1,13 +1,14 @@
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
 using BlazingShop.Data;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
+
+var cnnStr = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<AppDbContext>(x => x.UseSqlServer(cnnStr));
 
 var app = builder.Build();
 
