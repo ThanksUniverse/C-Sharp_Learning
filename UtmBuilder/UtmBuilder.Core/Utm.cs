51a392d8c2fffaa794b3e727463f697bc083f07e
Pedro <ThanksUniverse333@outlook.com>
2023-08-29 21:56:30 -0300

    Organizando Entidades e Value Objects

---
+++ UtmBuilder/UtmBuilder.Core/Utm.cs
@@ -0,0 +1,13 @@
+﻿using UtmBuilder.Core.ValueObjects;
+
+namespace UtmBuilder.Core;
+
+public class Utm
+{
+    public Url Url { get; set; }
+    public string Source { get; set; }
+    public string Medium { get; set; }
+    public string Name { get; set; }
+    public string Term { get; set; }
+    public string Content { get; set; }
+}
---
+++ UtmBuilder/UtmBuilder.Core/ValueObjects/Url.cs
@@ -0,0 +1,6 @@
+﻿namespace UtmBuilder.Core.ValueObjects;
+
+public class Url : ValueObject
+{
+    
+}
