bab32791278a615e5d07f694f4d486ed42714f0b
Pedro <ThanksUniverse333@outlook.com>
2023-09-24 20:51:56 -0300

    CQRS && Balta

--- TodoProject/Todo.Domain.Infra/Contexts/TodoContext.cs
+++ TodoProject/Todo.Domain.Infra/Contexts/TodoContext.cs
@@ -1,6 +1,23 @@
-﻿namespace Todo.Domain.Infra.Contexts;
+﻿using Microsoft.EntityFrameworkCore;
+using Todo.Domain.Entities;
 
-public class TodoContext
+namespace Todo.Domain.Infra.Contexts;
+
+public class DataContext : DbContext
 {
-    
+    public DataContext(DbContextOptions<DataContext> options) : base(options)
+    {
+    }
+
+    public DbSet<TodoItem> Todos { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<TodoItem>().Property(x => x.Id);
+        modelBuilder.Entity<TodoItem>().Property(x => x.User).HasMaxLength(120);
+        modelBuilder.Entity<TodoItem>().Property(x => x.Title).HasMaxLength(160);
+        modelBuilder.Entity<TodoItem>().Property(x => x.Done);
+        modelBuilder.Entity<TodoItem>().Property(x => x.Date);
+        modelBuilder.Entity<TodoItem>().Property(x => x.User);
+    }
 }
